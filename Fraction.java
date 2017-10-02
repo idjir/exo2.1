3a3810084c562751f72329cefc113bfeb8df0e32
uvsq21400502 <hamad_idir@hotmail.fr>
2017-10-02 15:06:16 +0200

    commit

---
+++ Fraction.java
@@ -0,0 +1,33 @@
+
+/**
+ * Write a description of class Fraction here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Fraction
+{
+    // instance variables - replace the example below with your own
+    private double num,denum;
+
+    /**
+     * Constructor for objects of class Fraction
+     */
+    public Fraction(double num, double denum)
+    {
+        // initialise instance variables
+        this.num=num;
+        this.denum=denum;
+    }
+    public Fraction()
+    {
+    this(0,0);
+     }
+
+    @Override
+    public String toString()
+    {
+        // put your code here
+        return "num:" + double.valueof(num).toString() = " "+ "denum:" + double.valueof(denum).toString();
+    }
+}
